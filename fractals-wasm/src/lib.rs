e55e38cd1e1d04a4e42be56d0b303f4ff547c10d
Michal Bitnerowski <224261@edu.p.lodz.pl>
2021-04-25 21:16:43 +0200

    Added basic colouring

--- fractals-wasm/src/lib.rs
+++ fractals-wasm/src/lib.rs
@@ -1,14 +1,14 @@
 #![feature(wasm_simd)]
 mod math;
+mod color;
 
 extern crate wasm_bindgen;
 extern crate web_sys;
 extern crate console_error_panic_hook;
-use std::panic;
 
 use wasm_bindgen::prelude::*;
 use math::{FractalGenerator, Generator, FractalConfig};
-use math::mandelbrot::MandelbrotGenerator;
+use color::{GrayscaleTransformation, ColorTransformation};
 use web_sys::window;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -23,34 +23,24 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-
-    console_error_panic_hook::set_once();
-
-    let perf = window()
-        .expect("Window expected")
-        .performance()
-        .expect("Performance expected");
-
-    let time = perf.now();
-
-    let gen = Generator::MANDELBROT.create();
+pub fn generate_test() -> Vec<u8> {
 
     let conf = FractalConfig {
-        start_re: -1.45,
+        start_re: -1.0,
         end_re: 2.0,
-        re_steps: 2000,
+        re_steps: 1250,
 
-        start_im: 0.0043,
+        start_im: -1.0,
         end_im: 1.0,
-        im_steps: 1000,
+        im_steps: 62,
 
-        max_iters: 500
+        max_iters: 500,
     };
 
-    let result = gen.generate(&conf);
+    let colors = GrayscaleTransformation {};
 
+    let gen = Generator::MANDELBROT.create();
+    let data = gen.generate(&conf);
 
-    alert(&(perf.now() - time).to_string());
-    alert(&result[0].iterations.to_string());
+    return colors.transform(data, conf.max_iters);
 }
